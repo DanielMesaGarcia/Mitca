665c8cb0b700e899fd3680ad97a22da9fcbe2a61
DanielMesaGarcia <daniwlmesa12@gmail.com>
2023-10-31 16:01:27 +0000

    LOGIN HECHO

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  // Contenido y lógica específicos para la página de inicio de sesión
+  return (
+    <div>
+      <h2>Home</h2>
+      {/* Agrega el contenido y la lógica adicional aquí */}
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Input, Button } from 'antd';
+import './Login.css'; // Archivo de estilos CSS personalizado
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="background-image">
+        <img src="/img/couple.jpg" alt="Background" className="bg-img" />
+      </div>
+      <div className="login-form">
+        <h2>Bienvenido a la Maratón</h2>
+        <Input placeholder="Correo electrónico" className="input" />
+        <Input.Password placeholder="Contraseña" className="input" />
+
+            {/* PROVISIONAL HASTA QUE AÑADA SISTEMA DE TOKENS Y LOGINS */}
+
+        <Link to="/home">
+            <Button type="primary" className="login-button">
+            Iniciar sesión
+            </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
