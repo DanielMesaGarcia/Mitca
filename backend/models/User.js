ad215a6914a9db43cd29b88d6534204ac7b3203f
DanielMesaGarcia <daniwlmesa12@gmail.com>
2023-11-06 19:31:56 +0000

    encrypted

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,40 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    _id: { 
-        type: String, 
-        required: true, 
-        unique: true, 
-        validate: {
-          validator: function(v) {
-            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v); // Expresión regular para validar el formato de correo electrónico
-          },
-          message: props => `${props.value} no es un correo electronico valido!`
-        }
+  _id: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
       },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
   password: { type: String, required: true },
   DNI: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   phone: { type: String, required: true },
-  admin: { type: Boolean, default:false}
+  admin: { type: Boolean, default: false },
 });
 
-const user = mongoose.model('user', userSchema);
-module.exports = user;
+// Hash the password before saving
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+  next();
+});
+
+// Compare the entered password with the hashed password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -5,10 +5,11 @@ const UserController = require('../controllers/UserController');
 userRouter.route('/')
   .post(UserController.createUser)
   .get(UserController.getUsers);
-
+  
 userRouter.route('/:_id')
   .get(UserController.getUserById)
   .put(UserController.updateUser)
   .delete(UserController.deleteUser);
 
+  userRouter.route('/login').post(UserController.login);
 module.exports = userRouter;
