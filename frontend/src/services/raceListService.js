f125640a570f5c8173471e35a895527055c9cf3d
Daniel Mesa <Daniwlmesa12@gmail.com>
2023-11-14 13:48:18 +0000

    Cool crud in the making

--- frontend/src/services/raceListService.js
+++ frontend/src/services/raceListService.js
@@ -11,9 +11,39 @@ const getRaces = async () => {
     throw new Error(`Error fetching races: ${error.message}`);
   }
 };
+const createRace = async (raceData) => {
+  try {
+    const response = await axios.post(API_URL, raceData);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error creating race: ${error.message}`);
+  }
+};
+
+const updateRace = async (raceId, updatedData) => {
+  try {
+    const response = await axios.put(`${API_URL}/${raceId}`, updatedData);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error updating race: ${error.message}`);
+  }
+};
+
+const deleteRace = async (raceId) => {
+  try {
+    const response = await axios.delete(`${API_URL}/${raceId}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error deleting race: ${error.message}`);
+  }
+};
 
 const RaceListService = {
   getRaces,
+  createRace,
+  updateRace,
+  deleteRace,
 };
+
 
 export default RaceListService;
