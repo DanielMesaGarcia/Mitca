c6dec582292174e5f95b9acb86ef55f157b8d42f
Daniel Mesa <Daniwlmesa12@gmail.com>
2023-11-19 20:08:47 +0000

    Race images working

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -109,7 +109,7 @@ const Home = () => {
             <List.Item key={race._id} onClick={() => handleCardClick(race._id)}>
               <Card title={race._id} style={{ width: '100%' }}>
                 <img
-                  src={`../../../backend/public/images/${race.filename}`}
+                  src={`http://localhost:3001/images/${race.filename}`}
                   alt={race.filename}
                   style={{ width: '100%', height: 'auto' }}
                 />
